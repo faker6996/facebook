ddf282c3475d225e361d220603a329bf3b70e9de
bachtv <bach.tv2000@gmail.com>
2025-05-25 21:46:16 +0700

    config custom style

---
+++ components/icons/BreadcrumIcon.tsx
@@ -0,0 +1,21 @@
+// components/icons/ChevronRight.tsx
+import React from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
+
+const ChevronRight = (props: IconProps) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="h-4 w-4"
+    {...props}
+  >
+    <path d="M9 18l6-6-6-6" />
+  </svg>
+);
+
+export default ChevronRight;
