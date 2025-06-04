1f6ec7e4c074473daf435a543d309715ae0b6b53
bachtv <bach.tv2000@gmail.com>
2025-06-04 21:49:15 +0700

    feat: config z-index

--- lib/constants/enum.ts
+++ lib/constants/enum.ts
@@ -1,8 +1,15 @@
 // lib/constants/http-method.ts
 export enum HTTP_METHOD_ENUM {
-    GET = 'GET',
-    POST = 'POST',
-    PUT = 'PUT',
-    DELETE = 'DELETE',
-  }
-  
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+
+export enum Z_INDEX_LEVEL {
+  BASE = 100, // base layout, header, fixed nav
+  DROPDOWN = 1000, // dropdown, popover, select
+  TOOLTIP = 2000, // tooltip
+  DIALOG = 10000, // modal, popup
+  LOADING = 100000, // global loading, overlay
+}
