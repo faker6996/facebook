b52c7aff472a9cc96768cfdaaafbd789815c7aa4
bachtv <bach.tv2000@gmail.com>
2025-06-29 13:50:41 +0700

    fix: status type

--- components/messenger/MessageList.tsx
+++ components/messenger/MessageList.tsx
@@ -25,12 +25,12 @@ const MessageList = ({ messages, senderId, onRetrySend }: MessageListProps) => {
             className={cn(
               "max-w-[80%] w-fit break-words rounded-xl px-3 py-2 text-sm shadow-md flex flex-col",
               isSender ? "ml-auto bg-blue-600 text-white" : "mr-auto bg-gray-200 text-gray-800",
-              msg.status === "failed" && "bg-red-200 text-red-800 opacity-90"
+              msg.status === "Failed" && "bg-red-200 text-red-800 opacity-90"
             )}
           >
             <p className="text-pretty">{msg.content}</p>
             <div className="flex items-center self-end mt-1.5 gap-2">
-              {msg.status === "failed" ? (
+              {msg.status === "Failed" ? (
                 <>
                   <span className="text-xs font-semibold">Gửi lỗi</span>
                   <button onClick={() => onRetrySend(msg)} className="text-xs font-bold hover:underline">
@@ -43,7 +43,7 @@ const MessageList = ({ messages, senderId, onRetrySend }: MessageListProps) => {
                   {isSender && (
                     <MessageStatusIcon
                       status={msg.status}
-                      className={cn("size-4", isSender ? "text-white/80" : "text-gray-500", msg.status === "read" && "!text-cyan-300")}
+                      className={cn("size-4", isSender ? "text-white/80" : "text-gray-500", msg.status === "Read" && "!text-cyan-300")}
                     />
                   )}
                 </>
